40b66cbb1d41a272ab8f1d624ab94d5c63d53359
gabriel acer <gabrielaug4@gmail.com>
2024-11-17 04:36:03 -0300

    feat: add ai report generation

---
+++ app/(home)/_actions/generate-ai-report/schema.ts
@@ -0,0 +1,7 @@
+import { isMatch } from "date-fns";
+import { z } from "zod";
+
+export const generateAiReportSchema = z.object({
+  month: z.string().refine((value) => isMatch(value, "MM")),
+});
+export type GenerateAiReportSchema = z.infer<typeof generateAiReportSchema>;
